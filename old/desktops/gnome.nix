a9bbce359944abb25bdb4a6aa9775d6e27890321
MacKenzie779 <MacKenzie779@proton.me>
2024-03-13 16:24:57 +0100

    removed old stuff

--- old/desktops/gnome.nix
+++
@@ -1,7 +0,0 @@
-{ config, pkgs, ...} : 
-{
-	services.xserver.enable = true;
-	services.xserver.xkb.layout = "de";
-	services.xserver.displayManager.gdm.enable = true;
-	services.xserver.desktopManager.gnome.enable = true;
-}
--- old/system/sound.nix
+++
@@ -1,4 +0,0 @@
-{ pkgs, ... }: {
-	sound.enable = true;
-	hardware.pulseaudio.enable = true;
-}
