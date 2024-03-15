8a245774f098f6a6e430689a607421d1bf476aac
MacKenzie779 <MacKenzie779@proton.me>
2024-03-15 15:03:04 +0100

    added nivvm profile

---
+++ profiles/nixvm/configuration.nix
@@ -0,0 +1,84 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ pkgs, lib, systemSettings, userSettings, ... }:
+
+{
+  imports = [ 
+    ./hardware-configuration.nix
+    ../../system/boot/systemdboot.nix
+    ../../system/hardware/kernel.nix # Kernel config
+    ../../system/hardware/time.nix # Network time sync
+    ../../system/hardware/opengl.nix
+    #../../system/hardware/bluetooth.nix
+    (./. + "../../../system/wm"+("/"+userSettings.wm)+".nix") # My window manager
+    ../../system/app/prismlauncher.nix
+    ../../system/app/automount.nix # important for automounting devs like usb
+    ../../system/app/virtualization.nix
+    #( import ../../system/app/docker.nix {storageDriver = "btrfs"; inherit userSettings lib;} )
+    ../../system/style/stylix.nix
+  ];
+
+  # Ensure nix flakes are enabled
+  nix.package = pkgs.nixFlakes;
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  nixpkgs.config.allowUnfree = true;
+
+  # Networking
+  networking.hostName = systemSettings.hostname; # Define your hostname.
+  networking.networkmanager.enable = true; # Use networkmanager
+
+  security.polkit.enable = true;
+
+  # Timezone and locale
+  time.timeZone = systemSettings.timezone; # time zone
+  i18n.defaultLocale = systemSettings.locale_us;
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = systemSettings.locale_de;
+    LC_IDENTIFICATION = systemSettings.locale_de;
+    LC_MEASUREMENT = systemSettings.locale_de;
+    LC_MONETARY = systemSettings.locale_de;
+    LC_NAME = systemSettings.locale_de;
+    LC_NUMERIC = systemSettings.locale_de;
+    LC_PAPER = systemSettings.locale_de;
+    LC_TELEPHONE = systemSettings.locale_de;
+    LC_TIME = systemSettings.locale_de;
+  };
+
+  console.keyMap = "de";
+
+  # User account
+  users.users.${userSettings.username} = {
+    isNormalUser = true;
+    description = userSettings.name;
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" ];
+    packages = [];
+    uid = 1000;
+  };
+
+  # System packages
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    zsh
+    git
+    home-manager
+  ];
+
+  virtualization.virtualbox.guest.enable = true;
+
+  # I use zsh btw
+  environment.shells = with pkgs; [ zsh ];
+  users.defaultUserShell = pkgs.zsh;
+  programs.zsh.enable = true;
+
+  fonts.fontDir.enable = true;
+
+  # It is ok to leave this unchanged for compatibility purposes
+  system.stateVersion = "22.11";
+
+}
---
+++ profiles/nixvm/home.nix
@@ -0,0 +1,94 @@
+{ config, pkgs, stylix, userSettings, ... }:
+
+{
+
+  home.username = userSettings.username;
+  home.homeDirectory = "/home/"+userSettings.username;
+
+  programs.home-manager.enable = true;
+
+  imports = [
+    
+	  stylix.homeManagerModules.stylix	
+    (./. + "../../../user/wm"+("/"+userSettings.wm+"/"+userSettings.wm)+".nix")
+    ../../user/shell/sh.nix # My zsh and bash config
+    ../../user/shell/cli-collection.nix # Useful CLI apps
+    ../../user/app/ranger/ranger.nix # My ranger file manager config
+    ../../user/app/git/git.nix # My git config
+    (./. + "../../../user/app/browser"+("/"+userSettings.browser)+".nix") # My default browser selected from flake
+    ../../user/app/virtualization/virtualization.nix # Virtual machines
+    ../../user/style/stylix.nix # Styling and themes for my apps
+    #../../user/hardware/bluetooth.nix # Bluetooth
+  ];
+
+  home.stateVersion = "22.11"; # Please read the comment before changing.
+
+  home.packages = with pkgs; [
+    # Core
+    zsh
+    alacritty
+    brave
+    dmenu
+    rofi
+    git
+    vscode
+    discord
+
+    # Office
+    libreoffice-fresh
+    xournalpp
+    newsflash
+    gnome.nautilus
+    gnome.gnome-calendar
+    openvpn
+    #protonmail-bridge
+    texliveSmall
+
+    # Media
+    gimp
+    pinta
+    musikcube
+    vlc
+    yt-dlp
+    obs-studio
+    kdenlive
+    ffmpeg
+    spotify
+
+    marktext
+    rofi-power-menu
+  ];
+
+  xdg.enable = true;
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+    music = "${config.home.homeDirectory}/Media/Music";
+    videos = "${config.home.homeDirectory}/Media/Videos";
+    pictures = "${config.home.homeDirectory}/Media/Pictures";
+    templates = "${config.home.homeDirectory}/Templates";
+    download = "${config.home.homeDirectory}/Downloads";
+    documents = "${config.home.homeDirectory}/Documents";
+    desktop = null;
+    publicShare = null;
+    extraConfig = {
+      XDG_DOTFILES_DIR = "${config.home.homeDirectory}/.dotfiles";
+      XDG_ARCHIVE_DIR = "${config.home.homeDirectory}/Archive";
+      XDG_VM_DIR = "${config.home.homeDirectory}/Machines";
+      XDG_ORG_DIR = "${config.home.homeDirectory}/Org";
+      XDG_PODCAST_DIR = "${config.home.homeDirectory}/Media/Podcasts";
+      XDG_BOOK_DIR = "${config.home.homeDirectory}/Media/Books";
+    };
+  };
+  xdg.mime.enable = true;
+  xdg.mimeApps.enable = true;
+
+  home.sessionVariables = {
+    EDITOR = userSettings.editor;
+    SPAWNEDITOR = userSettings.spawnEditor;
+    TERM = userSettings.term;
+    BROWSER = userSettings.browser;
+  };
+
+}
+
